f74d591701b4247513d0285c0478be7b38f17aa2
Bebop28 <samomsebi28@gmail.com>
2018-01-20 13:49:16 +0100

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'articles/new'
 
   root 'static_pages#new'
   get '/about', to: 'static_pages#about'
   resources :articles 
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
