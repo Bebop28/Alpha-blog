53621c93e4aa6cb7452b399c865e577c7619e547
Bebop28 <samomsebi28@gmail.com>
2018-01-23 23:26:55 +0100

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update, :show] 
+  before_action :require_admin, only: [:destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5 )
   end
@@ -11,19 +14,19 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
   	else
   	  render 'new'
     end
   end
 
   def edit
-  	@user = User.find(params[:id])
+
   end
 
   def update
-  	@user = User.find(params[:id])
       if @user.update(user_params)
         flash[:success] = "Your account was updated successfully"
         redirect_to articles_path
@@ -33,12 +36,37 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
+
   private
-    def user_params
-    	params.require(:user).permit(:username, :email, :password)
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
+
+  def require_admin
+    if logged_in? && !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'articles/new'
-
   root 'static_pages#new'
+  get 'users/new'
+  get 'articles/new'
   get '/about', to: 'static_pages#about'
-  resources :articles 
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :articles 
   resources :users
 end
