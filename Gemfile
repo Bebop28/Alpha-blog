ae95a4e0ccb2648bd4a08bb70a8bcd52a9149254
Bebop28 <samomsebi28@gmail.com>
2018-02-01 12:10:19 +0100

    Category model, model and integration tests

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ end
 
 group :development do
   gem 'web-console',           '3.5.1'
-  gem 'listen',                '3.0.8'
+  gem 'listen',                '3.1.5'
   gem 'spring',                '2.0.2'
   gem 'spring-watcher-listen', '2.0.1'
 end
@@ -34,6 +34,7 @@ end
 
 group :production do
   gem 'pg', '0.18.4'
+  gem 'fog', '1.42'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#new'
-  get 'users/new'
-  get 'articles/new'
   get '/about', to: 'static_pages#about'
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  
   resources :articles 
-  resources :users
+  resources :users, except: [:new]
+  resources :categories, except: [:destroy]
 end
